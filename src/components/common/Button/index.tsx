442acc7585a767fa25ec358890f8758116557b95
Siong Esteban <siong.esteban@gmail.com>
2019-08-15 19:14:54 +0800

    Create defaultProps for Button component

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import './button.style.scss';
-
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
   <button className="button" {...rest}>
     {children}
   </button>
 );
+
+Button.defaultProps = {
+  type: 'button',
+};
+
+export { Button };
